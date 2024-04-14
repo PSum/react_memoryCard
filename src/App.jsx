7f0a0dd6fdd13dd084c296aaaf22e464ede65cd3
PSum <sum.philipp@gmail.com>
2024-04-14 20:52:38 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 const playingCards = [1, 2, 3, 4, 5, 6];
 
-function App() {
-  const [pressedNumbers, addPressedNumbers] = useState([]);
-
-  const handleButtonClick = (number) => {
-    addPressedNumbers([...pressedNumbers, number]);
-  };
-
-  function shuffle(array) {
-    let currentIndex = array.length;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-  }
-
+function Header(score, bestScore) {
   return (
-    <div>
-      {playingCards.map((cardNumber, index) => (
-        <button key={index} onClick={() => handleButtonClick(cardNumber)}>
-          {cardNumber}
-        </button>
-      ))}
-      <div>{pressedNumbers}</div>
-    </div>
+    <>
+      <h1>Memory Game</h1>
+      <div>
+        <div>Score: </div>
+        <div>Best score: </div>
+      </div>
+    </>
+  );
+}
+
+/* This doesent work yet */
+function MemoryPage() {
+  const cards = [1, 2, 3, 4, 5, 6];
+  return (
+    <>
+  cards.map((card) => {
+    // console.log({ card })
+    <div>test</div>
+  });
+</>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MemoryPage />
+    </>
   );
 }
 
