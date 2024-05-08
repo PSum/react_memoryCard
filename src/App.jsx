a865a165d0b59ff5912694c61a9d5d1ad986ff58
psum <sum.philipp@gmail.com>
2024-05-08 21:50:07 +0200

    MWP finished
    Game is working as intended. The game is reduced on its core concept.
    The memory game is played with Numbers between 1 and 10 instead of
    pictures. No css yet

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import {v4 as uuidv4} from  'uuid';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function PictureFrame({cards}) {
-  const listItems = cards.map(card => <button key={card}>{card}</button>)
-  return (<div>{listItems}</div>)
-}
-
 function PrintGameArray({gameArray}){
   const listItems = gameArray.map(item => <div key={uuidv4()}>{item.value}</div>)
   return(
@@ -23,21 +18,57 @@ function App() {
 const [score, setScore] = useState(0)
 const [bestScore, setBestScore] = useState(0)
 const [gameArray, setGameArray] = useState([])
+
+// This effect checks, if the gameArray array changes
+// If it does, checkForDouble is called
+useEffect(() => {
+  checkForDouble(gameArray);
+}, [gameArray]
+)
+
+// These functions handle the change of state
 function handleScore(){
   setScore(score + 1);
 }
+function resetScore(){
+  setScore(0);
+}
 function handleBestScore(){
-  setBestScore(bestScore + 1);
+  setBestScore(score-1);
 }
 function handleGameArray(value){
   setGameArray([...gameArray,{value}]);
+  handleScore();
 }
+function resetGameArray(){
+  setGameArray([]);
+}
+
+// This function checks, if an array, called gameArray, contains two identical values. If it does, it sets the highscore, resets the array and resets the score
+function checkForDouble(arr){
+  const transformedArray = arr.map(obj => obj.value);
+  const uniqueValues = new Set(transformedArray);
+  if (uniqueValues.size !== arr.length) {
+  resetGameArray();
+  handleBestScore();
+  resetScore();
+  }
+}
+
+//This function renders the buttons and gives them interactivity with the onClick function
+function PictureFrame({cards}) {
+  // This line of code shuffles the cards everytime the array gets rendered
+  const shuffledCards = cards.sort(() => Math.random() - 0.5);
+  const listItems = shuffledCards.map(card => <button key={card} onClick={() => handleGameArray(card)}>{card}</button>)
+  return (<div>{listItems}</div>)
+}
+
+// This is the content of the webpage
   return (
     <>
       <Header score={score} bestScore={bestScore} />
       <PictureFrame cards={cards} />
-      <button onClick={() => handleGameArray(2)}>Test button</button> {/* onclick pass function to button not value directly */}
-      <PrintGameArray gameArray={gameArray} />
+     {/* <PrintGameArray gameArray={gameArray} />*/}
     </>
   );
 }
