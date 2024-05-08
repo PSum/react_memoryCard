0c2a815c2c89d5f6346e7a79cdac68117f51fd93
PSum <sum.philipp@gmail.com>
2024-05-08 07:35:05 +0200

    added functionality
    Ein gameArray wurde als array mit state hinzugefügt. Darin werden die
    bereits benutzen Knöpfe gespeichert und momentan angezeigt.
    Es muss noch eine Funktion zum leeren der Liste hinzugefügt werden

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,38 @@ import {v4 as uuidv4} from  'uuid';
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function PictureFrame({cards}) {
-
-  const listItems = cards.map(card => <div key={uuidv4()}>{card}</div>)
+  const listItems = cards.map(card => <button key={card}>{card}</button>)
   return (<div>{listItems}</div>)
+}
+
+function PrintGameArray({gameArray}){
+  const listItems = gameArray.map(item => <div key={uuidv4()}>{item.value}</div>)
+  return(
+  <div>
+    {listItems.length ? listItems : <div></div>}
+  </div>
+  )
 }
 
 function App() {
 const [score, setScore] = useState(0)
 const [bestScore, setBestScore] = useState(0)
+const [gameArray, setGameArray] = useState([])
 function handleScore(){
   setScore(score + 1);
 }
 function handleBestScore(){
   setBestScore(bestScore + 1);
 }
+function handleGameArray(value){
+  setGameArray([...gameArray,{value}]);
+}
   return (
     <>
       <Header score={score} bestScore={bestScore} />
       <PictureFrame cards={cards} />
-      <button onClick={handleScore}>Add to Score test</button>
-      <button onClick={handleBestScore}>Add to best Score test</button>
+      <button onClick={() => handleGameArray(2)}>Test button</button> {/* onclick pass function to button not value directly */}
+      <PrintGameArray gameArray={gameArray} />
     </>
   );
 }
